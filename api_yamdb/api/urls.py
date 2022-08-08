b79f63a8e75302cc429db6bf2eaec9776dcfeb75
Egor Remezov <mr.drode@gmail.com>
2022-08-08 15:10:25 +0300

    поправил путь api

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('redoc/', TemplateView.as_view(template_name='redoc.html'),
+    path('v1/redoc/', TemplateView.as_view(template_name='redoc.html'),
          name='redoc'
          ),
 ]
