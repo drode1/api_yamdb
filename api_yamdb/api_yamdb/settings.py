3890594debb5c002ff937748071d1f8e6ef51682
p_ziyaev <p0lzi@ya.ru>
2022-08-09 13:55:37 +0300

    Fix error in tests. install jwt & djoser

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'djoser',
     'django_filters',
     'api.apps.ApiConfig',
     'users.apps.UsersConfig',
--- tests/test_07_files.py
+++ tests/test_07_files.py
@@ -1,6 +1,7 @@
 import os
 
-from .conftest import MANAGE_PATH, project_dir_content, root_dir_content
+from .conftest import (
+    BASE_DIR, MANAGE_PATH, project_dir_content, root_dir_content)
 
 # проверяем, что в папки приложения api не находятся модели
 api_path = os.path.join(MANAGE_PATH, 'api')
@@ -21,7 +22,7 @@ assert filename in root_dir_content, (
     f'В корне проекта не найден файл `{filename}`'
 )
 
-with open(filename, 'r') as f:
+with open(os.path.join(BASE_DIR, filename), 'r') as f:
     file = f.read()
     assert file != default_md, (
         f'Не забудьте оформить `{filename}`'
