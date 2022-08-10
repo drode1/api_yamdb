fb8db258835cb1af2f174dcb6b3fa8781176b8fa
Egor Remezov <mr.drode@gmail.com>
2022-08-10 20:28:01 +0300

    Вью, сериализаторы, урлы и доступы

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from rest_framework.routers import DefaultRouter
+
+from api.views import (UserViewSet, RegisterUser,
+                       ObtainUserToken)
+
+router_v1 = DefaultRouter()
+router_v1.register('v1/users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('v1/redoc/', TemplateView.as_view(template_name='redoc.html'),
-         name='redoc'
-         ),
+         name='redoc'),
+    path('v1/auth/signup/', RegisterUser.as_view(), name='user-signup'),
+    path('v1/auth/token/', ObtainUserToken.as_view(), name='user-token'),
 ]
+
+urlpatterns += router_v1.urls
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,70 @@
-from django.shortcuts import render
+import random
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, filters, permissions, status
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from api.serializers import (UserSerializer, UserRegisterSerializer,
+                             ObtainUserTokenSerializer)
+from permissions import IsCustomAdminUser
+
+User = get_user_model()
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """ Вью сет для взаимодействия с пользователями с помощью админа. """
+
+    serializer_class = UserSerializer
+    filter_backends = [filters.SearchFilter]
+    permission_classes = (permissions.IsAuthenticated, IsCustomAdminUser,)
+    lookup_field = 'username'
+    lookup_value_regex = '[\w.@+-]{1,150}'
+    search_fields = ('username',)
+    queryset = User.objects.all()
+
+
+class RegisterUser(CreateAPIView):
+    """ Вью для самостоятельной регистрации пользователей. """
+
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        serializer = UserRegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            # Генерируем пятизначный код
+            confirmation_code = random.randint(10000, 99999)
+            User.objects.get_or_create(**serializer.validated_data,
+                                       confirmation_code=confirmation_code)
+            # Отправляем код на почту пользователя
+            send_mail(
+                'Confirmation code',
+                f'Your confirmation code is {confirmation_code}',
+                None,
+                [serializer.validated_data.get('email')],
+                fail_silently=False,
+            )
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ObtainUserToken(CreateAPIView):
+    """ Вью для получения JWT токена пользователем. """
+
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        serializer = ObtainUserTokenSerializer(data=request.data)
+        if serializer.is_valid():
+            user = get_object_or_404(
+                User, username=serializer.data.get('username'))
+            code = serializer.data.get('confirmation_code')
+            # Проверяем, что код совпадает отправленный пользователем
+            # и сгенерированный автоматически совпадает
+            if user.confirmation_code == code:
+                token = RefreshToken.for_user(user)
+                return Response({'access': token.access_token})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
