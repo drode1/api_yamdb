4a3f4c1ec6ae026bcdec2d5405de4bd1f613a81e
Egor Remezov <mr.drode@gmail.com>
2022-08-13 11:47:29 +0300

    откатил правки

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -8,7 +8,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.generics import CreateAPIView
-from rest_framework.permissions import (IsAuthenticatedOrReadOnly)
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
